0aeb26c0925407e41dcf97ade6848c389b27943d
binay <binay885b@gmail.com>
2026-03-12 01:24:25 +0530

    update login response format and change cookie settings to 'sameSite: none' for improved compatibility

--- Backend/src/controllers/auth.controller.js
+++ Backend/src/controllers/auth.controller.js
@@ -52,6 +52,7 @@ export const signup = async (req, res) => {
 
 export const login = async (req, res) => {
   const { email, password } = req.body;
+
   try {
     const user = await User.findOne({ email });
 
@@ -60,17 +61,18 @@ export const login = async (req, res) => {
     }
 
     const isPasswordCorrect = await bcrypt.compare(password, user.password);
+
     if (!isPasswordCorrect) {
       return res.status(400).json({ message: "Invalid credentials" });
     }
 
     generateToken(user._id, res);
 
-    res.status(200).json(user, {
+    res.status(200).json({
+      user,
       message: "Login completed",
     });
   } catch (error) {
-    console.log("Error in login controller", error.message);
     res.status(500).json({ message: "Internal Server Error" });
   }
 };
